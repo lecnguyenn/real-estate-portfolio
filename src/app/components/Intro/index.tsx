21d1ece1f82ba9c9eaba9741299839a3504012ef
nguyen.le <nguyen.le@ntq-solution.com.vn>
2024-11-11 18:00:20 +0700

    feature: update banner

--- src/app/components/Intro/index.tsx
+++ src/app/components/Intro/index.tsx
@@ -1,85 +1,64 @@
 'use client'
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css/autoplay';
-import 'swiper/css/effect-fade';
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+import 'swiper/css/autoplay'
+import 'swiper/css/effect-fade'
 
-import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { useEffect, useRef } from 'react'
+const imageBannerSource = [
+  'https://neckle-rtl-nextjs.vercel.app/assets/img/home2/bannar2-bg.png',
+  'https://neckle-rtl-nextjs.vercel.app/assets/img/home2/bannar2-bg2.png',
+  'https://neckle-rtl-nextjs.vercel.app/assets/img/home2/bannar2-bg3.png',
+]
 
 const Intro = () => {
-    return (
-        <>
-            <div className='relative'>
-                <Swiper
-                    spaceBetween={30}
-                    effect={'fade'}
-                    navigation={true}
-                    pagination={{
-                        clickable: true,
-                        // bulletClass: 'w-9'
-                    }}
-                    autoplay={{
-                        delay: 1500,
-                        disableOnInteraction: false
-                    }}
-                    loop={true}
-                    modules={[EffectFade, Navigation, Pagination, Autoplay]}
-                >
-                    <SwiperSlide className='w-[1223px]'>
-                        <div className="bg-cover bg-no-repeat"
-                            style={{
-                                backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0.15) 100%), url('https://neckle-rtl-nextjs.vercel.app/assets/img/home2/bannar2-bg.png')`
-                            }}
-                        >
-                            <div className='relative flex flex-wrap pr-[5%]'>
-                                <div>
-                                    <div className='max-w-[625px] w-full'>
-                                        <h2 className='font-bold not-italic leading-tight text-[75px]'>Neckle Valley Real Estate</h2>
-                                        <p>To help neckle valley Real estate</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className='w-[1223px]'>
-                        <div
-                            className="bg-cover bg-no-repeat"
-                            style={{
-                                backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0.15) 100%), url('https://neckle-rtl-nextjs.vercel.app/assets/img/home2/bannar2-bg2.png')`
-                            }}
-                        >
-                            <div className='relative flex flex-wrap'>
-                                <div>
-                                    <div className='max-w-[625px] w-full'>
-                                        <h2 className='font-bold not-italic leading-tight text-[75px]'>Neckle Valley Real Estate</h2>
-                                        <p>To help neckle valley Real estate</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className='w-[1223px]'>
-                        <div
-                            className="bg-cover bg-no-repeat"
-                            style={{
-                                backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0.15) 100%), url('https://neckle-rtl-nextjs.vercel.app/assets/img/home2/bannar2-bg3.png')`
-                            }}
-                        >
-                            <div className='relative flex flex-wrap'>
-                                <div>
-                                    <div className='max-w-[625px] w-full'>
-                                        <h2 className='font-bold not-italic leading-tight text-[75px]'>Neckle Valley Real Estate</h2>
-                                        <p>To help neckle valley Real estate</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                </Swiper>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="relative">
+        <Swiper
+          spaceBetween={30}
+          effect={'fade'}
+          navigation={true}
+          pagination={{
+            clickable: true,
+            renderBullet: (_, className) => {
+              return '<span class=\"' + className + '\">' + '</span>';
+            },
+            // bulletClass: 'flex gap-[30px] h-2 w-2 bg-white',
+            // bulletActiveClass: ''
+          }}
+          className='flex'
+          autoplay={false}
+          loop={false}
+          modules={[EffectFade, Navigation, Pagination, Autoplay]}
+        >
+          {imageBannerSource.map((item, key) => (
+            <SwiperSlide key={key}>
+              <div
+                className=" pt-[334px] pb-[264px] pr-[3%] bg-cover bg-no-repeat"
+                style={{
+                  backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0.15) 100%), url(${item})`,
+                }}
+              >
+                <div className="relative flex flex-wrap flex-row-reverse pr-[5%]">
+                  <div>
+                    <div className="max-w-[625px] w-full text-white text-right">
+                      <h2 className="font-bold not-italic leading-tight text-[75px] p-[-250px] select-none mb-[15px]">
+                        NewDay Valley Real Estate
+                      </h2>
+                      <p className='font-normal text-lg leading-normal mb-10'>.To help newday company stand out and make an impression on potential.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  )
 }
-export default Intro;
+export default Intro
--- src/app/components/layout/Header/index.tsx
+++ src/app/components/layout/Header/index.tsx
@@ -1,48 +1,68 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
+
+// import Phone from '../../../../../public/images/file.svg'
 
 const Header = () => {
-
-    return (
-        <div className="absolute w-full z-[777] top-0 left-0">
-            <div className="border-b border-[hsla(0,0%,100%,.15)]">
-                <div className="flex items-center justify-between py-3.5 px-44">
-                    <div className="flex items-center gap-5">
-                        <div className="flex">
-                            <span>To More Inquri</span>
-                            <h6>+84392854780</h6>
-                        </div>
-                        <Image src={'@images/file.svg'} alt="phone number" width={28} height={28} />
-                    </div>
-                    <div>
-                        <Image src={'https://neckle-rtl-nextjs.vercel.app/assets/img/home2/icon/home2-logo.svg'} alt="logo" width={250} height={250} />
-                    </div>
-                </div>
+  return (
+    <div className="absolute w-full z-[777] top-0 left-0">
+      <div className="border-b border-[hsla(0,0%,100%,.15)]">
+        <div className="flex items-center justify-between py-3.5 px-44">
+          <div className="flex items-center gap-5">
+            <div className="relative flex flex-col font-semibold text-end ">
+              <span className="text-[14px] text-[hsla(0,0%,100%,.8)] mb-[5px]">To More Inquri</span>
+              <h6 className="text-base leading-[20px] text-[#ffff] hover:text-[#efb93f] transition duration-500">
+                <a href="">+990-737 621 432</a>
+              </h6>
+                 <span className="absolute right-[-10px] top-1/2 h-[32px] w-[1px] bg-white/50 transform -translate-y-1/2"></span>
             </div>
-            <header className="flex w-full z-2 py-3.5 px-44 border-b border-[hsla(0,0%,100%,.15)]">
-                {/* <div> */}
-                <ul className="flex items-center list-none m-0 p-0 my-0 mx-auto uppercase text-white text-[13px]">
-                    <li>
-                        <Link href="/" className="py-[15px] px-5 font-semibold">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/" className="py-[15px] px-5 font-semibold">Contact us</Link>
-                    </li>
-                    <li>
-                        <Link href="/" className="py-[15px] px-5 font-semibold">Property</Link>
-                    </li>
-                    <li>
-                        <Link href="/" className="py-[15px] px-5 font-semibold">For rent</Link>
-                    </li>
-                    <li>
-                        <Link href="/" className="py-[15px] px-5 font-semibold">For Sale</Link>
-                    </li>
-                </ul>
-                {/* </div> */}
-            </header>
+            {/* <Image src={'../../../../../public/images/file.svg'} alt="phone number" width={28} height={28} /> */}
+            <span>phone</span>
+            {/* <Phone /> */}
+          </div>
+          <div>
+            <Image
+              src={'https://neckle-rtl-nextjs.vercel.app/assets/img/home2/icon/home2-logo.svg'}
+              alt="logo"
+              width={250}
+              height={250}
+            />
+          </div>
         </div>
-    )
-
+      </div>
+      <header className="flex w-full z-2 py-3.5 px-44 border-b border-[hsla(0,0%,100%,.15)]">
+        {/* <div> */}
+        <ul className="flex items-center list-none m-0 p-0 my-0 mx-auto uppercase text-white text-[13px]">
+          <li>
+            <Link href="/" className="py-[15px] px-5 font-semibold">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="py-[15px] px-5 font-semibold">
+              Contact us
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="py-[15px] px-5 font-semibold">
+              Property
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="py-[15px] px-5 font-semibold">
+              For rent
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="py-[15px] px-5 font-semibold">
+              For Sale
+            </Link>
+          </li>
+        </ul>
+        {/* </div> */}
+      </header>
+    </div>
+  )
 }
 
-export default Header;
+export default Header
